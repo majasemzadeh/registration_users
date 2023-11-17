83f8c71ad6d5601dc0eb7e34e592763fff5af00a
majasemzadeh <ma.jasemzadeh@gmail.com>
2023-11-18 02:18:04 +0330

    add the app cek eritor corsheadt and registration app

--- registertion_users/settings.py
+++ registertion_users/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'corsheaders',
+
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'registertion_users.urls'
@@ -99,6 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CKEDITOR_CONFIGS = {
+    'default': {
+        'width': '100%',
+    },
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
