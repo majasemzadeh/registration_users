b0b3211e17fd682bb2663f145d7e8c74462538d8
majasemzadeh <ma.jasemzadeh@gmail.com>
2023-11-18 02:19:06 +0330

    register table in admin panel

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from registration.models import UserData, EmailStatus, EmailTemplate
+
 # Register your models here.
+admin.site.register(UserData)
+admin.site.register(EmailStatus)
+admin.site.register(EmailTemplate)
